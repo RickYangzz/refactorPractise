71ef8c6883a796691b1bfef8de05f5b81e202e30
yangzhenze <chenyusheng1992@163.com>
2023-02-22 15:54:03 +0800

    styles: 调整代码布局

--- 11.1_SeparateQueryFromModifier.ts
+++ 11.1_SeparateQueryFromModifier.ts
@@ -2,13 +2,12 @@
  * 将查询函数和修改函数分离
  *
  * Separate Query from Modifier
- * 
+ *
  * 规则：任何有返回值的函数，都不应该有看得到的副作用（尽量遵守，但不能绝对遵守）
- * 
+ *
  * 明确表现出“有副作用”和“无副作用”两种函数的差异，是一个很好的想法
- * 
+ *
  */
-
 
 /**
  * 重构前
@@ -27,9 +26,9 @@ function getTotalOutstandingAndSendBill() {
 
 /**
  * 重构后
- * 
+ *
  * 第一个属于查询函数，第二个属于修改函数
- * 
+ *
  * 函数分离后，函数更加符合职责单一原则
  *
  * @returns
@@ -42,8 +41,7 @@ function sendBill() {
   emailGateway.send(formatBill(customer));
 }
 
-
-/** 
+/**
  * ------------- 以下是准备数据 -------------
  */
 
@@ -58,16 +56,16 @@ const emailGateway = {
 };
 
 const customer = {
-    invoices: [
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-      { amount: 3 },
-    ],
-  };
+  invoices: [
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+    { amount: 3 },
+  ],
+};
