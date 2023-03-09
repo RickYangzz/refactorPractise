0d8bec8f40cab7eeb1fda66def5755b1a7d8aeda
rick_yang <781195098@qq.com>
2023-03-09 09:08:25 +0800

    style: 格式化代码

--- 11.3_RemoveFlagArgument.ts
+++ 11.3_RemoveFlagArgument.ts
@@ -11,14 +11,14 @@
 function _deliveryDate(anOrder, isRush) {
   if (isRush) {
     let deliveryTime;
-    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    if (['MA', 'CT'].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (['NY', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 2;
     else deliveryTime = 3;
     return anOrder.placedOn.plusDays(1 + deliveryTime);
   } else {
     let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    if (['MA', 'CT', 'NY'].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (['ME', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 3;
     else deliveryTime = 4;
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
@@ -34,16 +34,16 @@ function deliveryDate(anOrder, isRush) {
 
 function rushDeliveryDate(anOrder) {
   let deliveryTime;
-  if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-  else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  if (['MA', 'CT'].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (['NY', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 2;
   else deliveryTime = 3;
   return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
 function regularDeliveryDate(anOrder) {
   let deliveryTime;
-  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-  else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+  if (['MA', 'CT', 'NY'].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (['ME', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 3;
   else deliveryTime = 4;
   return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
